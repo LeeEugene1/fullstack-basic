0d80d8f343142ea8de5c1d675565e42067f7cd82
eugene <uwm1004@gmail.com>
2022-01-31 23:55:50 +0900

    대댓글 스키마와 스키마 목록

--- server/mongoose/schema/index.js
+++ server/mongoose/schema/index.js
@@ -1,6 +1,16 @@
 // 여러개의 스키마 관리
-const Article = require('./article')
+const Article = require('./article')//사용자가 작성한 게시글
+const Board = require('./board')//게시판
+const Comment = require('./comment')//게시글 안에 있는 댓글
+const Company = require('./company')//회사정보
+const Reply = require('./reply')//게사글안에 있는 대댓글
+const User = require('./user')//사용자 정보
 
 module.exports = {
     Article,
+    Board,
+    Comment,
+    Company,
+    Reply,
+    User,
 }
